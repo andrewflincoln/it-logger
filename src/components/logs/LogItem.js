62165cc3d75a1e88995ded3fa2908b4b80c400bb
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-12-10 15:10:14 -0800

    logs update in db

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Moment from 'react-moment'
-import { deleteLog} from '../../actions/logActions'
+import { deleteLog, setCurrent} from '../../actions/logActions'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log.id)
   }
@@ -15,7 +15,9 @@ const LogItem = ({ log, deleteLog }) => {
       <div>
         <a href='#edit-log-modal' className={`modal-trigger ${ 
           log.attention ? 'red-text' : 'blue-text'
-        }`}>{log.message}</a>
+        }`} onClick={() => setCurrent(log)}>
+          {log.message}
+        </a>
         <br/>
         <span className="grey-text">
           <span className="black-text">ID #{log.id} </span>
@@ -34,9 +36,10 @@ const LogItem = ({ log, deleteLog }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
-  deleteLog: PropTypes.func.isRequired
+  deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 }
 
 
 
-export default connect(null, {deleteLog})(LogItem)
+export default connect(null, {deleteLog, setCurrent})(LogItem)
