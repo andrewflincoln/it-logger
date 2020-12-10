5d37e98de98c7a9c34453a7a1915d90f06b452d9
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-12-09 17:05:29 -0800

    delete logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types'
 
 // export const getLogs = () => {
 //   return async dispatch => {
@@ -30,11 +30,54 @@ export const getLogs = () => async dispatch => {
         type: LOGS_ERROR,
         payload: error.response.data
       })
-    }
-
-  
+    } 
 }
 
+//Add log
+export const addLog = (log) => async dispatch => {
+  try {
+    setLoading()
+    
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json()
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    })
+  } 
+}
+
+//Delete Log
+export const deleteLog = (id) => async dispatch => {
+  try {
+    setLoading()
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE'
+    })
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
+    })
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    })
+  } 
+}
 
 //set loading to true
 export const setLoading = () => {
